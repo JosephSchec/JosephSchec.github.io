8dd8603545e8cc3002804842d5de800c51856cef
Joseph-Schechter <shecky522@gmail.com>
2021-09-30 16:50:12 -0400

    updated some stuff added js file and project tab

---
+++ main.js
@@ -0,0 +1,12 @@
+(function(){
+    'use strict';
+    document.getElementById('snobz').addEventListener('click',()=>{
+        window.open("https://snobz.vercel.app/","_blank")
+    });
+    document.getElementById(' snobzGit').addEventListener('click',()=>{
+        window.open("https://github.com/JosephSchec/Snobz","_blank")
+    });
+   
+
+
+}());
