3396b6775a1f8472acb11ccb6e9ceb5d4915df41
Joseph-Schechter <shecky522@gmail.com>
2022-05-16 16:35:01 -0400

    added zmanim apk finally

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -3,11 +3,14 @@ import useGitHub from '../../HelperFunctions/useGitHub'
 import styles from '../../Styles/Projects/Project.module.css'
 import { FiExternalLink } from 'react-icons/fi'
 import { AiFillGithub } from 'react-icons/ai'
+import { saveAs } from 'file-saver';
+import { BsDownload } from 'react-icons/bs'
+
 const QRCode = require('qrcode.react');
 
 export default function Project(props) {
-    const { projectTitle, qrCodeTo, github, repository } = props;
-
+    const { projectTitle, qrCodeTo, github, repository, downloadApk, downloadLinkName } = props;
+    const download = () => saveAs(downloadApk, downloadLinkName)
     const readMe = useGitHub(repository)
 
     return (
@@ -23,7 +26,14 @@ export default function Project(props) {
                         <a href={qrCodeTo} target={'_blank'} rel="noreferrer" className={styles.siteButton}><FiExternalLink size={40} /></a>
                     }
                     <a href={github} target={'_blank'} rel="noreferrer" className={styles.github}><AiFillGithub size={40} /></a>
-                </div>
+
+
+
+                    {downloadApk && downloadLinkName &&
+
+                        <button className={`animate-bounce clickable ${styles.siteButton}`} onClick={download}>   <BsDownload size={40} /></button>
+                    }</div>
+
                 <article className={styles.readMe}>{readMe}</article>
 
             </div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,14 +4,16 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Project from './Project';
 import styles from '../../Styles/Projects/Projects.module.css'
+import zmanim from '../../Assets/zmanim.apk'
+
 import useScroll from '../../HelperFunctions/useScroll';
 const settings = {
   infinite: true,
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 3000
+  //autoplay: true,
+  //autoplaySpeed: 3000
 };
 
 export default function Projects() {
@@ -27,7 +29,7 @@ export default function Projects() {
             <Project projectTitle='Snobz Original-Version' qrCodeTo='https://snobz.vercel.app/' github='https://github.com/JosephSchec/Snobz' repository='Snobz' />
             <Project projectTitle='Snobz2 Webpack-Version' qrCodeTo='https://snobz2.vercel.app/' github='https://github.com/JosephSchec/Snobz2' repository='Snobz2' />
             <Project projectTitle='Snobz React-Version' qrCodeTo='https://snobzz.vercel.app/' github='https://github.com/JosephSchec/snobz-react' repository='snobz-react' />
-            <Project projectTitle='Zmanim' github='https://github.com/JosephSchec/zmanim' repository='zmanim' />
+            <Project projectTitle='Zmanim' github='https://github.com/JosephSchec/zmanim' repository='zmanim' downloadApk={zmanim} downloadLinkName='zmanim.apk' />
             <Project projectTitle='Portfolio' github='https://github.com/JosephSchec/josephschec.github.io' repository='josephschec.github.io' />
 
           </Slider>
