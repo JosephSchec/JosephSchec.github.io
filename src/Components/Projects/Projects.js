f8837c141e0c7c4ebffedfcbbe78e9d907e2ca75
Joseph-Schechter <shecky522@gmail.com>
2022-05-16 16:36:38 -0400

    put back autoscroll

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -12,8 +12,8 @@ const settings = {
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
-  //autoplay: true,
-  //autoplaySpeed: 3000
+  autoplay: true,
+  autoplaySpeed: 3000
 };
 
 export default function Projects() {
