5c788a3f54d3eaa3e5a4b7b1547584048abbd348
Joseph-Schechter <shecky522@gmail.com>
2022-07-05 16:37:39 -0400

    projects update

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -9,6 +9,7 @@ import zmanim from '../../Assets/zmanim.apk'
 import useScroll from '../../HelperFunctions/useScroll';
 const settings = {
   infinite: true,
+  dots: true,
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
@@ -26,12 +27,14 @@ export default function Projects() {
       <div className={opacity ? '@apply opacity-0' : styles.center}>
         <div className={styles.carousel}>
           <Slider {...settings}>
+
             <Project projectTitle='Snobz Original-Version' qrCodeTo='https://snobz.vercel.app/' github='https://github.com/JosephSchec/Snobz' repository='Snobz' />
+            <Project projectTitle='Java Notepad Editor' github='https://github.com/JosephSchec/java-notepad-editor' repository='java-notepad-editor' />
             <Project projectTitle='Snobz2 Webpack-Version' qrCodeTo='https://snobz2.vercel.app/' github='https://github.com/JosephSchec/Snobz2' repository='Snobz2' />
             <Project projectTitle='Snobz React-Version' qrCodeTo='https://snobzz.vercel.app/' github='https://github.com/JosephSchec/snobz-react' repository='snobz-react' />
             <Project projectTitle='Zmanim' github='https://github.com/JosephSchec/zmanim' repository='zmanim' downloadApk={zmanim} downloadLinkName='zmanim.apk' />
             <Project projectTitle='Portfolio' github='https://github.com/JosephSchec/josephschec.github.io' repository='josephschec.github.io' />
-            <Project projectTitle="davenfor" qrCodeTo='https://www.davenfor.us' repository='private'   />
+            <Project projectTitle="davenfor" qrCodeTo='https://www.davenfor.us' repository='private' />
           </Slider>
         </div>
       </div>
